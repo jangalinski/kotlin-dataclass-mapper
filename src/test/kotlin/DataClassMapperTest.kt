ce027b83f6a71b41a3454e63687879b9410e737f
Richard Capraro <richard.capraro@saagie.com>
2020-01-25 16:28:56 +0100

    Add target parameter supplier
    targetParameterSupplier allows setting a default value for a target constructor param which is not present in the source dataclass

--- src/test/kotlin/DataClassMapperTest.kt
+++ src/test/kotlin/DataClassMapperTest.kt
@@ -11,15 +11,27 @@ internal class DataClassMapperTest {
   data class BazIn(val foo: FooIn)
 
   data class FooOut(val name: String)
+  data class FooOut2(val name: String, val age: Int, val active: Boolean)
   data class BarOut(val name: String, val foos: Set<FooOut>)
   data class BazOut(val foo: FooOut)
 
 
   val fooMapper = DataClassMapper<FooIn, FooOut>()
+  var fooWithTargetParamsMapper = DataClassMapper<FooIn, FooOut2>()
+    .targetParameterSupplier("active") { true }
+    .targetParameterSupplier(FooOut2::age) { 65 }
+
   val bazMapper = DataClassMapper<BazIn, BazOut>()
     .register("foo", fooMapper)
-  val barMapper = DataClassMapper<BarIn,BarOut>()
+
+  val bazMapperAlternative = DataClassMapper<BazIn, BazOut>()
+    .register(BazIn::foo, fooMapper)
+
+  val barMapper = DataClassMapper<BarIn, BarOut>()
     .register("foos", setMapper(fooMapper))
+
+  val barMapperAlternative = DataClassMapper<BarIn, BarOut>()
+    .register(BarIn::foos, setMapper(fooMapper))
 
   @Test
   fun `simple mapping foo`() {
@@ -28,8 +40,15 @@ internal class DataClassMapperTest {
   }
 
   @Test
+  fun `simple mapping foo with default param in target`() {
+    assertThat(fooWithTargetParamsMapper(FooIn("kermit")))
+      .isEqualTo(FooOut2("kermit", 65, true))
+  }
+
+  @Test
   fun `transitive mapping baz(foo)`() {
     assertThat(bazMapper(BazIn(FooIn("piggy"))))
+      .isEqualTo(bazMapperAlternative(BazIn(FooIn("piggy"))))
       .isEqualTo(BazOut(FooOut("piggy")))
   }
 
@@ -38,7 +57,10 @@ internal class DataClassMapperTest {
     val inValue = BarIn("kermit", setOf(FooIn("piggy")))
 
     val outValue = barMapper(inValue)
+    val outValue2 = barMapperAlternative(inValue)
 
-    assertThat(outValue).isEqualTo(BarOut("kermit", setOf(FooOut("piggy"))))
+    assertThat(outValue)
+      .isEqualTo(outValue2)
+      .isEqualTo(BarOut("kermit", setOf(FooOut("piggy"))))
   }
 }
